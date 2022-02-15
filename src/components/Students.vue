8321b76fab909513db2787298fe59a98a09be65e
Williker Carmo <carmo.williker@gmail.com>
2022-02-15 15:59:11 +0000

    Student component

---
+++ src/components/Students.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <h1>{{ title }}</h1>
+    <input type="text" placeholder="Student's name" v-model="name" />
+
+    <table border="1px">
+      <thead>
+        <th>Id</th>
+        <th>Name</th>
+        <th>Actions</th>
+      </thead>
+      <tbody>
+        <tr>
+          <td>1</td>
+          <td>Williker</td>
+          <td>
+            <button class="btn" @click="Remove()">Remove</button>
+          </td>
+        </tr>
+        <tr>
+          <td>2</td>
+          <td>Terezinha</td>
+          <td>
+            <button class="btn" @click="Remove()">Remove</button>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Students",
+  data() {
+    return {
+      title: "Student",
+      name: "Students",
+    };
+  },
+  props: {},
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
