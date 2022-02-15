df8e1b03d6627bd965d6f3eaac1abdf71307bbe1
Williker Carmo <carmo.williker@gmail.com>
2022-02-15 16:21:04 +0000

    Styles Table

--- src/App.vue
+++ src/App.vue
@@ -5,17 +5,53 @@
 </template>
 
 <script>
-import Students from './components/Students.vue'
+import Students from "./components/Students.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    Students
-  }
-}
+    Students,
+  },
+};
 </script>
 
 <style>
+@import url("https://fonts.googleapis.com/css?family=Montserrat:400,700");
+
+body {
+  background-color: #eee;
+  font-family: "Montserrat", sans-serif;
+  display: grid;
+  justify-items: center;
+}
+body,
+html {
+  margin: 0;
+  height: 100%;
+}
 #app {
+}
+
+table {
+  margin: 0px;
+  padding: 0px;
+  list-style-type: none;
+  width: 100%;
+}
+table tr td {
+  padding: 20px;
+  font-size: 1.3em;
+  background-color: #e0edf4;
+  margin-bottom: 2px;
+  color: #3e5252;
+}
+table thead th {
+  background-color: rgb(184, 208, 216) !important;
+  font-size: 1.2em;
+  padding: 10px 0px;
+  text-align: center !important;
+}
+.colPequeno {
+  width: 5%;
 }
 </style>
